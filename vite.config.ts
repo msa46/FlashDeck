25ba39a68d33bf5d8bb9e2c130e46bc32dffbd93
msa46 <mohamad.sanaye462@gmail.com>
2023-12-21 17:06:56 +0330

    FIX: Fix the problem with pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
-
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.svg','apple-touch-icon.png'],
+  manifest: {
+    name: 'FlashDeck',
+    short_name: 'FlashDeck',
+    theme_color: '#ffffff',
+    icons:[
+      {
+        src: 'android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src:'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      }
+    ]
+  }
+}
+pwaOptions.registerType = 'autoUpdate'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA(pwaOptions)
   ],
 })
