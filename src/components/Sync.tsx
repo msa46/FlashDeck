777c3091b2d2d38b1ede42460892ba19a80c7a8f
msa46 <mohamad.sanaye462@gmail.com>
2024-01-06 14:30:29 +0330

    FEAT: Add sync and retriev capabilities

--- src/components/Sync.tsx
+++ src/components/Sync.tsx
@@ -1,19 +1,35 @@
 import {
     useMutation,
+    useQuery,
   } from '@tanstack/react-query'
-import { css } from "../../styled-system/css"
+import { css } from '@shadow-panda/styled-system/css'
 import axios from 'axios'
-import { useState } from 'react'
-import { vstack } from '../../styled-system/patterns'
+import { useEffect, useState } from 'react'
+import { vstack } from '@shadow-panda/styled-system/patterns'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from './ui/alert-dialog'
+  
 
 
 const Sync = () => {
     const baseUrl = import.meta.env.VITE_API_URL + "/api/flashDeck/"
-    const deckUrl = baseUrl + localStorage.getItem("pid")
-    const [newPid, setNewPid] = useState(localStorage.getItem("sync") || "create")
-    // const [prevSyncState, setPrevSyncState] = useState("create")
+    const deckUrl = baseUrl + "/" + localStorage.getItem("pid")
+    const [newPid, setNewPid] = useState(localStorage.getItem("retrieveId") || "")
+    const [prevSyncState, setPrevSyncState] = useState(localStorage.getItem("sync") || "")
     const [url, setUrl] = useState(baseUrl)
     const [method, setMethod] = useState("POST")
+    const [isModalOpen, setModalOpen] = useState(false);
+    
+
    
     const syncMutation = useMutation({
         mutationFn: () => {
@@ -44,32 +60,99 @@ const Sync = () => {
         setUrl(deckUrl)
         setMethod("PATCH")
     }
+    
+    const {refetch} = useQuery({
+        queryKey:["flashCards"], 
+        queryFn: async () => {
+            const response = await axios.get(baseUrl + "/" + newPid, {validateStatus : false})
+            if (response.status === 200) {
+                localStorage.setItem("flashCards", JSON.stringify(response.data.flashCards))
+            }
+            
+            return response.data
+        },
+        enabled: false
+    })
+
     const onSyncClick = () => {
         if (localStorage.getItem("sync") === "retrieve"){
-            console.log("warn")
+            if (prevSyncState === "retrieve"){
+                refetch()
+            }
+
         }
         else {
             syncMutation.mutate()
         }
     }
+    const checkToOpen = () => {
+        if (localStorage.getItem("sync") === "retrieve" && prevSyncState !== "retrieve"){
+            setModalOpen(modal => !modal)
+        }
+    }
 
+    const onModalAccept = () => {
+        setPrevSyncState("retrieve")
+        refetch()
+    }
     
     return (
         <div className={vstack({})}>
-            <button 
-            onClick={onSyncClick}
-            className={css({
-                bg: "teal.700",
-                p: "2",
-                w: "full",
-                borderRadius: "lg",
-                textAlign:"center",
+            <AlertDialog open={isModalOpen} onOpenChange={checkToOpen} >
+                <AlertDialogTrigger asChild>
+                 <button 
+                    onClick={onSyncClick}
+                    className={css({
+                        bg: "teal.700",
+                        p: "2",
+                        w: "full",
+                        borderRadius: "lg",
+                        textAlign:"center",
 
-                _hover: {bg: 'teal.600'}
-            })}
-            >
-               Sync
-            </button>
+                        _hover: {bg: 'teal.600'}
+                    })}
+                    >
+                    Sync
+                </button>
+                </AlertDialogTrigger>
+                <AlertDialogContent className={css({
+                    shadow: "sm",
+                    bg: "stone.800",
+                    color: "white",
+                    borderColor: "stone.600",
+                })}>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>You are going to retrieve a flashDeck</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action overwrites your current flashDeck. Are you sure you want to continue?
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                    <AlertDialogCancel
+                    className={css({
+                        bg: "stone.800",
+                        border: "none",
+                        _hover: {
+                            color: "white",
+                        },
+
+                    })}
+                    >
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                    className={css({
+                        bg: "zinc.500",
+                    })}
+                    onClick={onModalAccept}
+                    >
+                        Continue
+                    </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
+            
             <input 
             type="text" 
             value={newPid} 
@@ -83,6 +166,7 @@ const Sync = () => {
                 textAlign:"center",
             })}
             />
+            
         </div>
     )
 }
