18157fc6014a6a9a93ba2058013ac86c84242d08
msa46 <mohamad.sanaye462@gmail.com>
2024-01-08 11:44:22 +0330

    FEAT: Add clipboaerd functionality

--- src/components/Sync.tsx
+++ src/components/Sync.tsx
@@ -4,8 +4,9 @@ import {
   } from '@tanstack/react-query'
 import { css } from '@shadow-panda/styled-system/css'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { vstack } from '@shadow-panda/styled-system/patterns'
+import { useState } from 'react'
+import { vstack, hstack } from '@shadow-panda/styled-system/patterns'
+import { ClipboardCopy } from 'lucide-react'
 import {
     AlertDialog,
     AlertDialogAction,
@@ -17,7 +18,11 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
   } from './ui/alert-dialog'
-  
+import { Button } from './ui/button'
+import { Label } from './ui/label'
+
+import { Tooltip } from 'react-tooltip' 
+
 
 
 const Sync = () => {
@@ -64,8 +69,9 @@ const Sync = () => {
     const {refetch} = useQuery({
         queryKey:["flashCards"], 
         queryFn: async () => {
-            const response = await axios.get(baseUrl + "/" + newPid, {validateStatus : false})
+            const response = await axios.get(baseUrl + "/" + newPid, {validateStatus : () => false})
             if (response.status === 200) {
+                localStorage.setItem("retrieveId", response.data.pId)
                 localStorage.setItem("flashCards", JSON.stringify(response.data.flashCards))
             }
             
@@ -97,77 +103,111 @@ const Sync = () => {
     }
     
     return (
-        <div className={vstack({})}>
-            <AlertDialog open={isModalOpen} onOpenChange={checkToOpen} >
-                <AlertDialogTrigger asChild>
-                 <button 
-                    onClick={onSyncClick}
-                    className={css({
-                        bg: "teal.700",
-                        p: "2",
-                        w: "full",
-                        borderRadius: "lg",
-                        textAlign:"center",
+            <div className={vstack({})}>
+                <AlertDialog open={isModalOpen} onOpenChange={checkToOpen} >
+                        <AlertDialogTrigger asChild>
+                        <button 
+                            onClick={onSyncClick}
+                            data-tooltip-id="sync"
+                            data-tooltip-content="Sync, post or retrieve your flash cards"
+                            className={css({
+                                bg: "teal.700",
+                                p: "2",
+                                w: "50%",
+                                borderRadius: "lg",
+                                textAlign:"center",
 
-                        _hover: {bg: 'teal.600'}
-                    })}
-                    >
-                    Sync
-                </button>
-                </AlertDialogTrigger>
-                <AlertDialogContent className={css({
-                    shadow: "sm",
-                    bg: "stone.800",
-                    color: "white",
-                    borderColor: "stone.600",
-                })}>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>You are going to retrieve a flashDeck</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            This action overwrites your current flashDeck. Are you sure you want to continue?
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                    <AlertDialogCancel
-                    className={css({
-                        bg: "stone.800",
-                        border: "none",
-                        _hover: {
+                                _hover: {bg: 'teal.600'}
+                            })}
+                            >
+                            Sync
+                        </button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent className={css({
+                            shadow: "sm",
+                            bg: "stone.800",
                             color: "white",
-                        },
+                            borderColor: "stone.600",
+                        })}>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>You are going to retrieve a flashDeck</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action overwrites your current flashDeck. Are you sure you want to continue?
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                            <AlertDialogCancel
+                            className={css({
+                                bg: "stone.800",
+                                border: "none",
+                                _hover: {
+                                    color: "white",
+                                },
 
-                    })}
-                    >
-                        Cancel
-                    </AlertDialogCancel>
-                    <AlertDialogAction
-                    className={css({
-                        bg: "zinc.500",
-                    })}
-                    onClick={onModalAccept}
-                    >
-                        Continue
-                    </AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
-
+                            })}
+                            >
+                                Cancel
+                            </AlertDialogCancel>
+                            <AlertDialogAction
+                            className={css({
+                                bg: "zinc.500",
+                            })}
+                            onClick={onModalAccept}
+                            >
+                                Continue
+                            </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                        
+                </AlertDialog>
             
-            <input 
-            type="text" 
-            value={newPid} 
-            onChange={(e) => setNewPid(e.target.value)} 
-            onBlur={() => localStorage.setItem("sync", "retrieve")}
-            className={css({
-                bg: "stone.500",
-                p: "2",
-                w: "full",
-                borderRadius: "lg",
-                textAlign:"center",
-            })}
-            />
-            
-        </div>
+                <input 
+                type="text" 
+                value={newPid} 
+                onChange={(e) => setNewPid(e.target.value)} 
+                onBlur={() => localStorage.setItem("sync", "retrieve")}
+                placeholder="Paste your pid here"
+                className={css({
+                    bg: "stone.500",
+                    p: "2",
+                    w: "full",
+                    borderRadius: "lg",
+                    textAlign:"left",
+                })}
+                />
+                <Label className={hstack({
+                    bg: "stone.600",
+                    p: "2",
+                    w: "full",
+                    borderRadius: "lg",
+                    justify: "space-between"
+                })}>
+                        {localStorage.getItem("retrieveId") !== null? 
+                        localStorage.getItem("retrieveId") : 
+                        localStorage.getItem("pid")}
+                        <Button 
+                        variant="outline" 
+                        size="icon"
+                        onClick={() =>  navigator.clipboard.writeText(localStorage.getItem("retrieveId") || localStorage.getItem("pid") || "")}
+                        className={css({
+                            bg: 'stone.600',
+                            color: "stone.200",
+                            height: "8",
+                            width: "8",
+                            _hover: {
+                                color: "white",
+                                bg: "stone.500",
+                            }
+                        })}
+                        >
+                            <ClipboardCopy  className={css({
+                                height: "4",
+                                width: "4",
+                            })}/>
+                        </Button>
+                </Label>
+            <Tooltip id="sync" />
+            </div>
     )
 }
 
